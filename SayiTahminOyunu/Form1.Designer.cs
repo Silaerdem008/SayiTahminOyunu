86414f3ac5c856f9f1259f353829fe9070ed1b72
Sila <silaerdem68@gmail.com>
2025-10-21 22:50:22 +0300

    Sayı Tahmin Oyunu - İlk sürüm

---
+++ SayiTahminOyunu/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace SayiTahminOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(63, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(452, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TUTULAN SAYIYI TAHMİN EDİN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(64, 285);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gainsboro;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(181, 160);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 54);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "KONTROL ET";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(181, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 36);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Gainsboro;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(492, 356);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 54);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "YENİ OYUN";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(487, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "SÜRE :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(487, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "PUAN :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(576, 81);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 25);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(576, 119);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 25);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "label6";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(743, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ SayiTahminOyunu/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SayiTahminOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool oyunBitti = false;
+        Random rastgele = new Random();
+        private int puan = 100;
+        private int sure = 60;
+        int tutulanSayi, buyuk = 100, kucuk = 0;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tutulanSayi = rastgele.Next(100);
+            buyuk = 100;
+            kucuk = 0;
+            puan = 100;
+            sure = 60;
+            label2.Text = "SAYI " + kucuk + " VE " + buyuk + " ARASINDA ";
+            label5.Text = "  " + puan;
+            label6.Text = "  " + sure + " sn";
+
+            textBox1.Focus();
+            if (textBox1.Focus())
+            {
+                textBox1.Focus();
+            }
+            oyunBitti = false; // Yeni oyunda sıfırlıyoruz
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int tahmin;
+            if (!int.TryParse(textBox1.Text, out tahmin))
+            {
+                MessageBox.Show("Lütfen sadece sayı giriniz!");
+                textBox1.Clear();
+                textBox1.Focus();
+                return; // Hatalı giriş varsa metottan çık
+            }
+            timer1.Start();
+
+            if (tahmin == tutulanSayi)
+            {
+                timer1.Stop();
+                label2.Text = "BİLDİNİZ! Sayı: " + tahmin;
+                MessageBox.Show("TEBRİKLER!  Doğru Tahmin Ettiniz Puanınız: " + puan);
+                oyunBitti = true;
+            }
+            else if (tahmin > tutulanSayi)
+            {
+                buyuk = tahmin;
+                label2.Text = "Sayı " + kucuk + " Ve " + buyuk + " Arasında ";
+                puan -= 10;
+                label6.Text = " " + puan;
+                
+            }
+            else if (tahmin < tutulanSayi)
+            {
+                kucuk = tahmin;
+                label2.Text = "Sayı " + kucuk + " Ve " + buyuk + " Arasında ";
+                puan -= 10;
+                label6.Text = " " + puan;
+                
+            }
+            if (puan <= 0)
+            {
+                timer1.Stop();
+                MessageBox.Show("Puanınız Tükendi! Oyun Sona Erdi Doğru Sayı: " + tutulanSayi);
+                oyunBitti = true;
+            }
+            textBox1.Text = "";
+            if (textBox1.Focus())
+            {
+                textBox1.Focus();
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            tutulanSayi = rastgele.Next(100);
+            buyuk = 100;
+            kucuk = 0;
+            label2.Text = "Sayı " + kucuk + " Ve " + buyuk + " Arasında";
+            label5.Text = " " + puan;
+            label6.Text = " " + sure + " sn";
+
+            timer1.Interval = 1000;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sure--;
+            label5.Text = " " + sure + " sn";
+            if (sure == 0)
+            {
+                timer1.Stop();
+                MessageBox.Show("Süre Doldu! Doğru Sayı: " + tutulanSayi);
+                oyunBitti = true; // Süre doldu, oyun bitti
+            }
+        }
+
+
+    }
+}
